c13c891801cd29e7dbc5bdfd45d5be4c41236fb8
LordDrixx <dlord22398@gmail.com>
2023-06-20 04:02:13 -0700

    script

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
+*main - Entry point
+*Description: 'print alphabet in reverse'
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char low;
+int n = 122;
 
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-		putchar('\n');
-
-	return (0);
+while (n >= 97)
+{
+putchar(n);
+n--;
+}
+putchar('\n');
+return (0);
 }
